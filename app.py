6e9af5c36b89f5c8c6ed86bdb4fa2c4919e04a50
Sonca12 <bachquangtung162@gmail.com>
2025-02-07 08:36:35 +0700

    Fix cấu hình Flask cho Render

--- app.py
+++ app.py
@@ -14,5 +14,6 @@ def home():
     return render_template("index.html", messages=messages)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)  # Chạy trên localhost
+    app.run(host="0.0.0.0", port=5000)  # Chạy trên cổng 5000
+
  
